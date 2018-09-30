ed80b0ec55aaa13c67f3869f9384e56978563c1c
SuperDroidBond <superdroidbond@yahoo.com>
2018-10-01 00:19:42 +0530

    Make Flashing look better alligned

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -817,6 +817,9 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     # Stage 3/3: Make changes.
     script.Comment("Stage 3/3")
 
+  # Inform User
+  script.Print("BackupTool at Work");
+
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
@@ -832,6 +835,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.Mount("/system")
     script.RunBackup("backup")
     script.Unmount("/system")
+
+  script.Print("BackupTool Work Done");
 
   system_progress = 0.75
 
@@ -855,29 +860,29 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("         MSM-Xtended ROM by Team-Xtended       ");
   script.Print("***********************************************");
 
-  if target_info.GetBuildProp("ro.xtended.version") is not None:
-    buildid = target_info.GetBuildProp("ro.xtended.version")
-    androidver = target_info.GetBuildProp("ro.build.version.release")
-    buildtype = target_info.GetBuildProp("ro.build.type")
-    buildidn = target_info.GetBuildProp("ro.build.id")
-    buildday = target_info.GetBuildProp("ro.build.date")
-    securep = target_info.GetBuildProp("ro.build.version.security_patch")
-    device = target_info.GetBuildProp("ro.product.name")
-    manufacturer = target_info.GetBuildProp("ro.product.manufacturer")
+  xtndversion = target_info.GetBuildProp("ro.xtended.display.version")
+  androidver = target_info.GetBuildProp("ro.build.version.release")
+  buildtype = target_info.GetBuildProp("ro.build.type")
+  buildid = target_info.GetBuildProp("ro.build.id")
+  buildday = target_info.GetBuildProp("ro.build.date")
+  securep = target_info.GetBuildProp("ro.build.version.security_patch")
+  device = target_info.GetBuildProp("ro.product.name")
+  manufacturer = target_info.GetBuildProp("ro.product.manufacturer")
+
   script.Print("***********************************************");
-  script.Print(" ROM version    : %s"%(buildid));
+  script.Print(" ROM version      : %s"%(xtndversion));
   script.Print("");
   script.Print(" Android version  : %s"%(androidver));
   script.Print("");
-  script.Print(" Security patch : %s"%(securep));
+  script.Print(" Security patch   : %s"%(securep));
   script.Print("");
-  script.Print(" Build date     : %s"%(buildday));
+  script.Print(" Build date       : %s"%(buildday));
   script.Print("");
-  script.Print(" Build type     : %s"%(buildtype));
+  script.Print(" Build type       : %s"%(buildtype));
   script.Print("");
-  script.Print("***********************************************");
-  script.Print(" Device         : %s"%(device));
-  script.Print(" Manufacturer: %s"%(manufacturer));
+  script.Print(" Device           : %s"%(device));
+  script.Print("");
+  script.Print(" Manufacturer     : %s"%(manufacturer));
   script.Print("***********************************************");
 
   if OPTIONS.wipe_user_data:
